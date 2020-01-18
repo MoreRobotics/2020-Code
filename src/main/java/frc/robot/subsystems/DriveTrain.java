a6be84b7fda70d726ddebd874cd8dc4cc54a464d
MORE Robotics 1714 <robotics1714@gmail.com>
2020-01-18 14:58:01 -0600

    Additions to drive train and the drive command

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -7,18 +7,45 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+
 public class DriveTrain extends SubsystemBase {
+  WPI_TalonSRX falconFrontRight;
+  WPI_TalonSRX falconRearRight;
+  WPI_TalonSRX falconFrontLeft;
+  WPI_TalonSRX falconRearLeft;
+  SpeedControllerGroup rightDrive;
+  SpeedControllerGroup leftDrive;
+  DifferentialDrive drive;
+  XboxController operatorController;
   /**
    * Creates a new DriveTrain.
    */
-  public DriveTrain() {
-
+  public DriveTrain(int falconFrontRightID, int falconRearRightID, int falconFrontLeftID, int falconRearLeftID) {
+   falconFrontRight = new WPI_TalonSRX(falconFrontRightID);
+   falconRearRight = new WPI_TalonSRX(falconRearRightID);
+   rightDrive = new SpeedControllerGroup(falconFrontRight, falconRearRight);
+   falconFrontLeft = new WPI_TalonSRX(falconFrontLeftID);
+   falconRearLeft = new WPI_TalonSRX(falconRearLeftID);
+   leftDrive = new SpeedControllerGroup(falconFrontLeft, falconFrontRight);
+   drive = new DifferentialDrive(rightDrive, leftDrive);
+   
   }
-
+  public void Drive(/*double speed1, double speed2, double speed3, double speed4*/) {
+    /*falconFrontRight.set(ControlMode.Velocity, speed1);
+    falconRearRight.set(ControlMode.Velocity, speed2);
+    falconFrontLeft.set(ControlMode.Velocity, speed3);
+    falconRearLeft.set(ControlMode.Velocity, speed4);*/
+  }
   @Override
   public void periodic() {
+    drive.arcadeDrive(operatorController.getY(), operatorController.getX());
     // This method will be called once per scheduler run
   }
 }
