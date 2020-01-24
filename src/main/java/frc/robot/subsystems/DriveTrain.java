16df9ef8a03cc8323775bad648337f24b7536dd0
jmanrocks152 <jmanrocks152@gmail.com>
2020-01-23 18:11:05 -0600

    Drive

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -7,9 +7,16 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class DriveTrain extends SubsystemBase {
+  private TalonSRX motorLeft1 = new TalonSRX(0);
+  private TalonSRX motorLeft2 = new TalonSRX(1);
+  private TalonSRX motorRight1 = new TalonSRX(2);
+  private TalonSRX motorRight2 = new TalonSRX(3);
   /**
    * Creates a new DriveTrain.
    */
@@ -20,5 +27,14 @@ public class DriveTrain extends SubsystemBase {
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+  }
+
+  public void setLeftMotors(double speed) {
+    motorLeft1.set(ControlMode.PercentOutput, -speed); 
+    motorLeft2.set(ControlMode.PercentOutput, -speed);
+  }
+  public void setRightMotors(double speed) {
+    motorRight1.set(ControlMode.PercentOutput, speed); 
+    motorRight2.set(ControlMode.PercentOutput, speed);
   }
 }
