53c6f8ace0102c0d0bd278d802c67fe801d5dab4
Samuel Moore <1sammoore714@gmail.com>
2021-01-12 20:02:40 -0600

    Update RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -17,6 +17,7 @@ import edu.wpi.first.wpilibj.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.geometry.Translation2d;
 //import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.POVButton;
 import frc.robot.commands.*;
@@ -31,15 +32,16 @@ import frc.robot.Robot;
 import frc.robot.Constants;
 
 /**
- * This class is where the bulk of the robot should be declared.  Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls).  Instead, the structure of the robot
- * (including subsystems, commands, and button mappings) should be declared here.
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a "declarative" paradigm, very little robot logic should
+ * actually be handled in the {@link Robot} periodic methods (other than the
+ * scheduler calls). Instead, the structure of the robot (including subsystems,
+ * commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  //private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-  //Instantiates the subsystems
+  // private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  // Instantiates the subsystems
   Shooter shooter = new Shooter();
   DriveTrain driveTrain = new DriveTrain();
   Hopper hopper = new Hopper(shooter);
@@ -49,9 +51,10 @@ public class RobotContainer {
   Climber climber = new Climber();
   TrajectoryManager trajectoryManager = new TrajectoryManager();
 
-  //private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+  // private final ExampleCommand m_autoCommand = new
+  // ExampleCommand(m_exampleSubsystem);
 
-  //Instantiates the controllers and its associated buttons
+  // Instantiates the controllers and its associated buttons
   XboxController driverController = new XboxController(Constants.DRIVER_CONTROLLER_PORT);
   JoystickButton driverLBumper = new JoystickButton(driverController, XboxController.Button.kBumperLeft.value);
   JoystickButton driverRBumper = new JoystickButton(driverController, XboxController.Button.kBumperRight.value);
@@ -68,7 +71,7 @@ public class RobotContainer {
   POVButton operatorDPadUp = new POVButton(operatorController, 0);
 
   /**
-   * The container for the robot.  Contains subsystems, OI devices, and commands.
+   * The container for the robot. Contains subsystems, OI devices, and commands.
    */
   public RobotContainer() {
     // Configure the button bindings
@@ -76,30 +79,31 @@ public class RobotContainer {
   }
 
   /**
-   * Use this method to define your button->command mappings.  Buttons can be created by
-   * instantiating a {@link GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a
-   * {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   * Use this method to define your button->command mappings. Buttons can be
+   * created by instantiating a {@link GenericHID} or one of its subclasses
+   * ({@link edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then
+   * passing it to a {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
   private void configureButtonBindings() {
-    operatorAButton.whenHeld(new StartFlyWheelVelocityPID(shooter));
+    // operatorAButton.whenHeld(new StartFlyWheelVelocityPID(shooter));
     operatorLBumper.whenHeld(new IntakePowerCells(intake));
-    shooterHoodHandler();
-    hopperHandler();
-    controlPanelHandler();    
+    /*
+     * shooterHoodHandler(); hopperHandler();
+     */
+    controlPanelHandler();
   }
 
-  //Encapsulates the shooter hood commands
-  public void shooterHoodHandler() {
-    operatorBButton.whenPressed(new HoodAngleUp(shooter));
-    operatorXButton.whenPressed(new HoodAngleDown(shooter));
-  }
-
-  public void hopperHandler() {
-    operatorRBumper.whenHeld(new StagePowerCells(hopper));
-    operatorYButton.whenHeld(new FeedPowerCells(hopper));
-    operatorBackButton.whenHeld(new RemoveBallsFromTransporter(hopper));
-  }
+  // Encapsulates the shooter hood commands
+  /*
+   * public void shooterHoodHandler() { operatorBButton.whenPressed(new
+   * HoodAngleUp(shooter)); operatorXButton.whenPressed(new
+   * HoodAngleDown(shooter)); }
+   * 
+   * public void hopperHandler() { operatorRBumper.whenHeld(new
+   * StagePowerCells(hopper)); operatorYButton.whenHeld(new
+   * FeedPowerCells(hopper)); operatorBackButton.whenHeld(new
+   * RemoveBallsFromTransporter(hopper)); }
+   */
 
   public void controlPanelHandler() {
     driverLBumper.whenHeld(new TurnControlPanelLeft(controlPanel));
@@ -111,6 +115,10 @@ public class RobotContainer {
     operatorDPadUp.whenHeld(new IntakePullUp(intake));
   }
 
+  public void newShooter() {
+    operatorAButton.whenPressed(new ParallelCommandGroup(new StartFlyWheelVelocityPID(shooter),
+        new FeedPowerCells(hopper), new StagePowerCells(hopper)));
+  }
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
@@ -118,45 +126,35 @@ public class RobotContainer {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    var autoVoltageConstraint =
-        new DifferentialDriveVoltageConstraint(
-            new SimpleMotorFeedforward(Constants.ksVolts,
-                                       Constants.kvVoltSecondsPerMeter,
-                                       Constants.kaVoltSecondsSquaredPerMeter),
-            Constants.kDriveKinematics,
-            10);
+    var autoVoltageConstraint = new DifferentialDriveVoltageConstraint(new SimpleMotorFeedforward(Constants.ksVolts,
+        Constants.kvVoltSecondsPerMeter, Constants.kaVoltSecondsSquaredPerMeter), Constants.kDriveKinematics, 10);
 
     // Create config for trajectory
-    TrajectoryConfig config =
-        new TrajectoryConfig(Constants.kMaxSpeedMetersPerSecond,
-                             Units.metersToInches(Constants.kMaxAccelerationMetersPerSecondSquared))
+    TrajectoryConfig config = new TrajectoryConfig(Constants.kMaxSpeedMetersPerSecond,
+        Units.metersToInches(Constants.kMaxAccelerationMetersPerSecondSquared))
             // Add kinematics to ensure max speed is actually obeyed
             .setKinematics(Constants.kDriveKinematics)
             // Apply the voltage constraint
             .addConstraint(autoVoltageConstraint);
 
-    // An example trajectory to follow.  All units in meters.
+    // An example trajectory to follow. All units in meters.
     Trajectory exampleTrajectory = TrajectoryGenerator.generateTrajectory(
         // Start at the origin facing the +X direction
         new Pose2d(0, 0, new Rotation2d(0)),
         // Pass through these two interior waypoints, making an 's' curve path
-        List.of(
-            new Translation2d(1, 1),
-            new Translation2d(2, -1)
-        ),
+        List.of(new Translation2d(1, 1), new Translation2d(2, -1)),
         // End 3 meters straight ahead of where we started, facing forward
         new Pose2d(3, 0, new Rotation2d(0)),
         // Pass config
-        config
-    );
+        config);
 
-    switch(Robot.chosenAutoPath) {
+    switch (Robot.chosenAutoPath) {
       case 0:
         return new TestAutonomous(driveTrain, exampleTrajectory);
-      
+
       case 1:
         return new TestAutonomous(driveTrain, trajectoryManager.lineToTrench);
-      
+
       case 2:
         return new TestAutonomous(driveTrain, trajectoryManager.linetoThreeCenterBalls);
 
