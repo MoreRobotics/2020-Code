23be2f0e42e120edd5fbec5f3993e5dbba7c5c0a
Samuel Moore <1sammoore714@gmail.com>
2021-04-27 18:13:18 -0500

    Create photoeyetest.java

---
+++ src/main/java/frc/robot/commands/photoeyetest.java
@@ -0,0 +1,40 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.DriveTrain;
+
+public class photoeyetest extends CommandBase {
+  /** Creates a new photoeyetest. */
+  private final DriveTrain driveTrain;
+
+  public photoeyetest(DriveTrain driveTrain) {
+    this.driveTrain = driveTrain;
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    System.out.println(driveTrain.isBallPresent());
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
